e1a1f034d13417e8431db9b8dea3e24779ff1920
fozail-ahmed1 <fozailahmed02@gmail.com>
2023-04-05 00:46:10 +0530

    React-Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from "react";
-import { BrowserRouter,Route } from 'react-router-dom';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import About from "./Components/About";
 import Home from "./Components/Home";
+import Contact from "./Components/Contact";
 import Navbar from "./Components/Navbar";
+import Card from "./Components/Card";
 
 function App () {
     return(
     <BrowserRouter> 
         <div className="App">
             <Navbar />
-            <Route path='/' component={Home}/>
+            <Routes>
+                <Route exact path='/' element={<Home/>}/>
+                <Route exact path='/about' element={<About/>}/>
+                <Route exact path='/contact' element={<Contact/>}/>
+                <Route exact path='/card/:user' element={<Card/>}/>
+            </Routes>
         </div>
     </BrowserRouter>   
     );
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,16 +1,17 @@
 import React from "react";
+import {Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return(
         <nav className="ui raised very padded segment">
             <a className='ui teal inverted segment'>Gloria</a>
             <div className="ui right floated header">
-                <button className="ui button"><a href="/">Home</a></button>
-                <button className="ui button"><a href="src\Components\About.js">About</a></button>
-                <button className="ui button"><a href="src\Components\Contact.js">Contact</a></button>
+                <button className="ui button"><Link to="/">Home</Link></button>
+                <button className="ui button"><NavLink to="/about">About</NavLink></button>
+                <button className="ui button"><NavLink to="/contact">Contact</NavLink></button>
             </div>
         </nav>
-    )
+    );
 }
 
 export default Navbar;
