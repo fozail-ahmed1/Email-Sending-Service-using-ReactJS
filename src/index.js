861b8cf11bedc6867d470b1078127d6ab9b3d57a
fozail-ahmed1 <fozailahmed02@gmail.com>
2023-04-05 00:49:01 +0530

    First-App

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function getButtonText(){
+    return 'Hi There!';
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+    // const ButtonText = 'click me!';
+        return ( 
+        // <div> Hi There!</div>
+        <div>
+            <label htmlFor="name" className="label">Enter email</label>
+            <input id="name" type="text" />
+            <button style={{backgroundColor:'red', color:'white'}}>{getButtonText()}</button>
+        </div>
+        )
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
